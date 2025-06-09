0397219b4aa6311ddfdde2b297afd17b2b286fee
Prathyusha-10Sath <Prathyushaaa2006@gmail.com>
2025-06-09 18:39:54 +0530

    first commit

--- script.js
+++
@@ -1,8 +0,0 @@
-function showName(){
-     const name=document.getElementById('name').value 
-     const email=document.getElementsById('email').value
-   const registerno=document.getElementsById('registerno').value
-    const department=document.getElementsById('department').value
-    document.getElementsById('result').innerText=`
-    ${name} ${email} ${registerno} ${department}
-}
